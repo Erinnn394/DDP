cade3994e63f18b6827d8147af19f28e5d82dce1
Xiaoheng.Fei <95618653+Erinnn394@users.noreply.github.com>
2024-01-12 19:03:07 +0000

    Add files via upload

---
+++ happy.py
@@ -0,0 +1,12 @@
+def happy():
+    state = input("Are you happy?")
+    if state == "yes":
+        boring_lecture()
+        print("Glad you happy.^_^")
+    else:
+        return
+
+def boring_lecture():
+    return
+
+happy()
